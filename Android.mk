269649479fb571d3a03fbe3d56a942e48f3c0c05
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-13 01:41:24 +0530

    op5cam: Modity android makefile for this repository
    * Simply add OnePlusCamera to PRODUCT_PACKAGES to include
      the camera apk and all necessary dependencies.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,8 +6,25 @@
 # Find where we are
 LOCAL_PATH := $(call my-dir)
 
-# Check if the device is oneplus3
-ifeq ($(TARGET_DEVICE),oneplus3)
+### PRIV-APP
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := OnePlusCamera
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := system/priv-app/OnePlusCamera/OnePlusCamera.apk
+LOCAL_OVERRIDES_PACKAGES := Snap SnapdragonCamera Camera Camera2
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_SDK_VERSION := 23
+TARGET_PLATFORM := android-23
+APP_PLATFORM := android-23
+LOCAL_ADDITIONAL_DEPENDENCIES := OnePlusCameraLibs OnePlusCameraService securecamerasampleclient libfilter-sdk
+LOCAL_ADDITIONAL_DEPENDENCIES += lib-imscamera libimscamera_jni libimsmedia_jni
+
+include $(BUILD_PREBUILT)
 
 ### LIB and LIB64
 
@@ -22,12 +39,12 @@ LOCAL_PRELINK_MODULE := false
 ifdef TARGET_2ND_ARCH
 LOCAL_MULTILIB := both
 LOCAL_MODULE_PATH_64 := system/lib64
-LOCAL_SRC_FILES_64 := proprietary/lib64/libfilter-sdk.so
+LOCAL_SRC_FILES_64 := system/lib64/libfilter-sdk.so
 LOCAL_MODULE_PATH_32 := system/lib
-LOCAL_SRC_FILES_32 := proprietary/lib/libfilter-sdk.so
+LOCAL_SRC_FILES_32 := system/lib/libfilter-sdk.so
 else
 LOCAL_MODULE_PATH := system/lib
-LOCAL_SRC_FILES := proprietary/lib/libfilter-sdk.so
+LOCAL_SRC_FILES := system/lib/libfilter-sdk.so
 endif
 
 include $(BUILD_PREBUILT)
@@ -38,19 +55,16 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := securecamerasampleclient
-LOCAL_SRC_FILES := proprietary/bin/secure_camera_sample_client
+LOCAL_SRC_FILES := system/bin/secure_camera_sample_client
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_PREBUILT)
 
-
-### PRIV-APP
-
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := OnePlusCamera
+LOCAL_MODULE := OnePlusCameraService
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := proprietary/priv-app/OnePlusCamera/OnePlusCamera.apk
+LOCAL_SRC_FILES := system/priv-app/OnePlusCameraService/OnePlusCameraService.apk
 LOCAL_OVERRIDES_PACKAGES := Snap SnapdragonCamera Camera Camera2
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
@@ -59,7 +73,6 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_SDK_VERSION := 23
 TARGET_PLATFORM := android-23
 APP_PLATFORM := android-23
-LOCAL_ADDITIONAL_DEPENDENCIES := OnePlusCameraLibs
 
 include $(BUILD_PREBUILT)
 
@@ -107,12 +120,12 @@ LOCAL_PRELINK_MODULE := false
 ifdef TARGET_2ND_ARCH
 LOCAL_MULTILIB := both
 LOCAL_MODULE_PATH_64 := system/lib64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/lib-imscamera.so
+LOCAL_SRC_FILES_64 := system/vendor/lib64/lib-imscamera.so
 LOCAL_MODULE_PATH_32 := system/lib
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/lib-imscamera.so
+LOCAL_SRC_FILES_32 := system/vendor/lib/lib-imscamera.so
 else
 LOCAL_MODULE_PATH := system/lib
-LOCAL_SRC_FILES := proprietary/vendor/lib/lib-imscamera.so
+LOCAL_SRC_FILES := system/vendor/lib/lib-imscamera.so
 endif
 
 include $(BUILD_PREBUILT)
@@ -128,12 +141,12 @@ LOCAL_PRELINK_MODULE := false
 ifdef TARGET_2ND_ARCH
 LOCAL_MULTILIB := both
 LOCAL_MODULE_PATH_64 := system/lib64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libimscamera_jni.so
+LOCAL_SRC_FILES_64 := system/vendor/lib64/libimscamera_jni.so
 LOCAL_MODULE_PATH_32 := system/lib
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libimscamera_jni.so
+LOCAL_SRC_FILES_32 := system/vendor/lib/libimscamera_jni.so
 else
 LOCAL_MODULE_PATH := system/lib
-LOCAL_SRC_FILES := proprietary/vendor/lib/libimscamera_jni.so
+LOCAL_SRC_FILES := system/vendor/lib/libimscamera_jni.so
 endif
 
 include $(BUILD_PREBUILT)
@@ -149,13 +162,12 @@ LOCAL_PRELINK_MODULE := false
 ifdef TARGET_2ND_ARCH
 LOCAL_MULTILIB := both
 LOCAL_MODULE_PATH_64 := system/lib64
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libimsmedia_jni.so
+LOCAL_SRC_FILES_64 := system/vendor/lib64/libimsmedia_jni.so
 LOCAL_MODULE_PATH_32 := system/lib
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libimsmedia_jni.so
+LOCAL_SRC_FILES_32 := system/vendor/lib/libimsmedia_jni.so
 else
 LOCAL_MODULE_PATH := system/lib
-LOCAL_SRC_FILES := proprietary/vendor/lib/libimsmedia_jni.so
+LOCAL_SRC_FILES := system/vendor/lib/libimsmedia_jni.so
 endif
 
 include $(BUILD_PREBUILT)
-endif
