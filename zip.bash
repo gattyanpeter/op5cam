f456deac1e32b6ed171e49f832eac9f40bbadbfc
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-13 19:37:05 +0530

    zip.bash: Ignore the Android makefile
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- zip.bash
+++ zip.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 VERSION=OOS_Camera_OP5_Flashable_v0.4
-zip -0 -ur ${VERSION}.zip . -x ".git/*" "zip.bash" ".gitignore" "*.zip"
+zip -0 -ur ${VERSION}.zip . -x ".git/*" "zip.bash" ".gitignore" "*.zip" "Android.mk"
